b69748e48c7053d267edd6168e0e2d740ef12a7e
PRAKASH-WEBDEV <PRAKASH-WEBDEV@users.noreply.github.com>
2025-12-12 11:43:44 +0530

    now function chapter complete

--- function/02function.js
+++ function/02function.js
@@ -5,3 +5,22 @@ function calculatecartprice( var1,var2 , ...num1) {
     return num1
 }
 console.log(calculatecartprice(100,200,300,400,500));// the output is [300,400,500] because rest operator stores the remaining values in an array
+
+
+// the rest are return always array ...
+
+const user ={
+    username:"prince",
+    age:23
+}
+
+function handleObject(anyobj){
+    console.log(`Username is ${anyobj.username}  and the age is ${anyobj.age}okay`);
+
+
+}
+console.log(handleObject(user));// the return of this line is undefined ..
+
+
+
+
---
+++ function/03scope.js
@@ -0,0 +1,37 @@
+// it is a very imp topic ..for hadle the function and scope 
+
+let a = 10
+
+if( true ){
+ 
+ const b=20
+ var c= 30
+} // yeh jo curley braces hai voh hai scope or ..isko hum bolte hai BLOCK SCOPE  or iske bahar jo likhnete hai voh hai global scope 
+
+//  console.log(a)
+//  console.log(b)
+ console.log(c)
+
+ {}// it is a curley braces 
+
+ //var jo hai usko bahut kam use krte hai kyuki yeh scope ke bahar or ander dono jagaha access ho sakta hai .
+
+
+ //nested scope .......................
+
+ function one(){
+    const username="prince"
+
+    function two(){
+        const website= "google"
+
+        console.log(username);
+    }
+
+    // console.log(website)
+
+    two()
+ }
+
+
+ one()
